9f52aa2ee7a9120e6d7100e5e7d7fc5ae4315436
Sven <Sventenhaaf@users.noreply.github.com>
2024-05-01 21:49:51 +0200

    add favicon and title

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,13 +1,15 @@
-import { jsxRenderer } from 'hono/jsx-renderer'
+import { useEffect, useState } from "hono/jsx";
+import { jsxRenderer } from "hono/jsx-renderer";
 
 export const renderer = jsxRenderer(({ children, title }) => {
   return (
     <html>
       <head>
         <link href="/static/style.css" rel="stylesheet" />
+        <link rel="icon" type="image/svg+xml" href="/static/favicon.svg" />
         <title>{title}</title>
       </head>
       <body>{children}</body>
     </html>
-  )
-})
+  );
+});
