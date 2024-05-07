3c7e61fb477856f1293893b2fb3ac3bff9a3e0bf
Sven <Sventenhaaf@users.noreply.github.com>
2024-05-07 14:59:35 +0200

    more card styling

--- src/card.tsx
+++ src/card.tsx
@@ -2,6 +2,10 @@ import type { FC } from "hono/jsx";
 
 const IMG_WIDTH = 100;
 const IMG_HEIGHT = 100;
+
+const DOT_SIZE = 10;
+
+const COLORS = ["tomato", "#5566FF", "#34d399", "yellow"];
 interface Props {
   title: string;
   description: string;
@@ -9,18 +13,29 @@ interface Props {
 
 const Card: FC<Props> = ({ title, description }) => {
   return (
-    <div style=" background: white; border: 1px solid lightgray; padding: 10px; display: flex; gap: 10px;">
-      <img
-        width={IMG_WIDTH}
-        height={IMG_HEIGHT}
-        src="https://utfs.io/f/64e12e70-8861-44bb-8f2c-0d3d5d3aa4a3-eecii3.28.27.png"
-        alt="placeholder"
-      />
-      <div>
-        <h3 style="font-size: 18px; margin:0; font-weight: 500; margin-bottom:4px;">{title}</h3>
-        <p style="font-size: 14px; margin:0;">{description}</p>
+    <div style="border-radius: 4px; border: 1px solid black; padding: 10px; display: flex; flex-direction: column;">
+      <div style="display:flex; flex-direction: row; gap: 10px;">
+        <img
+          style="object-fit: contain;"
+          width={IMG_WIDTH}
+          height={IMG_HEIGHT}
+          src="https://utfs.io/f/64e12e70-8861-44bb-8f2c-0d3d5d3aa4a3-eecii3.28.27.png"
+          alt="placeholder"
+        />
+        <div style="display: flex; flex-direction:column;">
+          <h3 style="font-size: 18px; margin:0; font-weight: 500; margin-bottom:4px;">{title}</h3>
+          <p style="font-size: 14px; margin:0; flex:1;">{description}</p>
+          <div style={`display:flex; flex-direction:row; gap: ${DOT_SIZE}px; padding: 5px 0; align-items: center;`}>
+            {COLORS.map((color) => (
+              <div
+                style={`width: ${DOT_SIZE}px; height: ${DOT_SIZE}px; border-radius: 50%; background: ${color}`}
+              ></div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
+
     // <div style="border: 3px solid blue; display:flex; flex-direction:row; ">
     //   <div>
     //     <img
